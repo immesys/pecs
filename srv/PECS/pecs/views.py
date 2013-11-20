725fdb05c836e520140e9d1e2b1722903e924faa
bynoejos <joseph.bynoe@gmail.com>
2013-11-20 00:49:00 -0800

    Web Page after Mongodb :s

--- srv/PECS/pecs/views.py
+++ srv/PECS/pecs/views.py
@@ -1,7 +1,7 @@
 from pyramid.view import view_config
 
 
-@view_config(route_name='home', renderer='templates/mytemplate.pt')
+@view_config(route_name='home', renderer='pecs:templates/home.mako')
 def my_view(request):
     return {'project': 'PECS'}
     
